fb3061e2a8cb3f6284eb2dc94a6f29dd79a46dc2
Divyanshu Yadav <divyanshu.1810@gmaiil.com>
2023-01-16 18:41:22 +0530

    :bug: draggable button

--- components/message/icon.tsx
+++ components/message/icon.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Link from "next/link";
-import Draggable, { DraggableCore } from "react-draggable";
 function Icon() {
   return (
     <Link href="/message">
-      <Draggable>
-        <div className="fixed animate-pulse z-50 cursor-pointer right-6 bottom-6 lg:right-8 lg:bottom-8 hover:bg-red-500 duration-150 bg-red-800 rounded-full p-3">
-          <picture>
-            <img
-              width={35}
-              className=" -rotate-12 "
-              src="/messages/message.png"
-              alt="message-icon"
-            />
-          </picture>
-        </div>
-      </Draggable>
+      <div className="fixed z-50 cursor-pointer right-6 bottom-6 lg:right-8 lg:bottom-8 hover:bg-red-500 duration-150 bg-red-800 rounded-full p-3">
+        <picture>
+          <img
+            width={35}
+            className=" -rotate-12 "
+            src="/messages/message.png"
+            alt="message-icon"
+          />
+        </picture>
+      </div>
     </Link>
   );
 }
