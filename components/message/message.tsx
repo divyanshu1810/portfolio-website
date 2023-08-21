a6297f189212d2d350678295ce106d190344c957
Divyanshu Yadav <divyanshu.yadav@trainingmug.com>
2023-08-22 02:27:58 +0530

    feat: add own backend

--- components/message/message.tsx
+++ components/message/message.tsx
@@ -38,7 +38,7 @@ const Message: FunctionComponent = () => {
       validEmail(formData.email)
     ) {
       axios
-        .post(`/api/message`, {
+        .post(`${process.env.NEXT_PUBLIC_URL}`, {
           message: formData.message,
           email: formData.email,
           name: formData.name,
