4ebf5c3e095254209be30cfb37b30633dc3865cb
Divyanshu Yadav <divyanshu.yadav@trainingmug.com>
2023-08-22 02:25:18 +0530

    feat: add own backend

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -2,7 +2,7 @@
 import { client } from '@/utils/database'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -11,6 +11,15 @@ export default function handler(
             const collection = client.db("portfolio").collection("messages");
             collection.insertOne(req.body);
             res.status(200).json({success:true, message: "Message sent successfully" });
+        } catch (error) {
+            res.status(500).json({success:false, message: "Message not sent" });
+        }
+    }
+    if(req.method === "GET"){
+        try {
+            const collection = client.db("portfolio").collection("messages");
+            const messages = await collection.find({}).toArray();
+            res.status(200).json({success:true, message: "Message fetched successfully", data: messages });
         } catch (error) {
             res.status(500).json({success:false, message: "Message not sent" });
         }
